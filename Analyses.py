ed55a8b08c4f0def77923316a6f7957a091b7a2a
luciemallet <luciemallet@live.fr>
2017-02-03 17:33:52 +0100

    Màj 03/02
    - Suppression du kmeans/cah dans analyse
    - Nettoyage des codes
    - Graph 3D
    - Manque légende graph + stats sur les top

--- Analyses.py
+++ Analyses.py
@@ -4,128 +4,70 @@ Created on Fri Jan  6 09:23:46 2017
 
 @author: Lucie
 """
-#test
-## Librairies utilisées
+
+
+########## Librairies utilisées
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sn
 import pylab
-from sklearn.covariance import EllipticEnvelope
 from sklearn.preprocessing import scale
 from sklearn.decomposition import PCA
-from sklearn import cluster
 from mpl_toolkits.mplot3d import Axes3D
-from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
-
 
 
 ########## Lecture des données
 
 
 # Definition du chemin où sont situées les données :
-path = 'C:/Users/Richard/Documents/GitHub/Segmentation-multicanale2/Données'  
+path = 'C:/Users/Richard/Documents/GitHub/Segmentation-multicanale2/Données/v2'
 
 # Import des données
-quanti_trans = pd.read_csv(path + '/quanti_trans.csv',delimiter=",",dtype={"IDPART_CALCULE":object})
+quanti_trans = pd.read_csv(path + '/quanti_trans2.csv', delimiter=",", \
+                           dtype={"IDPART_CALCULE":object})
 types = quanti_trans.dtypes # Ok
 print(types)
-del(types)
+del types
 
 
 ########### Boxplot (rapide)
-
 data_boxplot = np.array(quanti_trans)
-data_boxplot = data_boxplot[:,1:43]
+data_boxplot = data_boxplot[:, 1:43]
 data_boxplot = data_boxplot.astype(np.float32)
 
-pylab.ylim([-0,20])
+pylab.ylim([-0, 20])
 plt.boxplot(data_boxplot)
-del(data_boxplot)
+del data_boxplot
 
 
 
+########## ACP
+base_test = quanti_trans.drop(['IDPART_CALCULE', 'Actionsd_MaBanque_3m', 'Lecture_mess_3m',
+                               'Ecriture_mess_3m'], axis=1)
 
-
-
-########## ACP 
-
+# Stats moyenne et ecart type
+mean = np.mean(base_test, axis=0)
+std = np.std(base_test, axis=0)
+stats = pd.concat([mean, std], axis=1)
+stats.columns = ['mean', 'std']
+del(mean, std)
 
 # Normaliser les données
-base_test = quanti_trans.drop(['IDPART_CALCULE'],axis=1)
 data_scale = pd.DataFrame(scale(base_test))
 data_scale.columns = [s + '_norm' for s in list(base_test.columns.values)]  # Renomer les colonnes
 
 
-## Code ACP (en utilisant sklearn, on peut utiliser matplotlib qui donne les mêmes résultats)
-
-pca = PCA(n_components=42)  # setup de l'ACP
-pcafit = pca.fit(data_scale) # application sur les données
-var = pca.explained_variance_ratio_
-
-## Graph de la variance expliquée par les composantes
-pylab.ylim([-0.01,0.3])
-pylab.xlim([-1,43])
-plt.ylabel('Part de la variance expliquée')
-plt.xlabel('Composantes')
-plt.plot(var,'bo')
-plt.show()
-
-## Graph de la variance cumulée expliquée
-plt.ylabel('Part de la variance expliquée')
-plt.xlabel('Composantes')
-plt.plot(np.cumsum(var))
-plt.show()
-
-## Nouvelle coordonnées
-data_coor = pd.DataFrame(pca.transform(data_scale))
-data_coor.columns = ["Comp_" + str(l) for l in list(range(1, 43, 1))] # Renomer les colonnes
-
-## Scatter plot sur les premiers plans de l'ACP
-
-pylab.ylim([-7,10])
-plt.boxplot(np.array(data_coor)[:,0:40]) # Variance des composantes : décroissante ok
-
-plt.scatter(np.array(data_coor)[:,0],np.array(data_coor)[:,1])
-plt.scatter(np.array(data_coor)[:,0],np.array(data_coor)[:,2])
-plt.scatter(np.array(data_coor)[:,1],np.array(data_coor)[:,2])
-
-# Graph des 3 premieres composantes 3D
-fig = plt.figure(1, figsize=(8, 6))
-ax = Axes3D(fig, elev=-150, azim=110)
-ax.scatter(np.array(data_coor)[:, 0], np.array(data_coor)[:, 1], np.array(data_coor)[:, 2],
-cmap=plt.cm.Paired)
-ax.set_title("ACP: trois premieres composantes")
-ax.set_xlabel("Comp1")
-ax.w_xaxis.set_ticklabels([])
-ax.set_ylabel("Comp2")
-ax.w_yaxis.set_ticklabels([])
-ax.set_zlabel("Comp3")
-ax.w_zaxis.set_ticklabels([])
-
-
-
-# 2eme essai ACP en enlevant les variables inintéressantes : 'nb_jours_CAEL_3m',nb_jours_MaBanque_3m,
-# Actionsd_MaBanque_3m,Lecture_mess_3m, Ecriture_mess_3m
-base_test2 =  base_test.drop(['nb_jours_CAEL_3m','nb_jours_MaBanque_3m','Actionsd_MaBanque_3m',
-'Lecture_mess_3m', 'Ecriture_mess_3m'], 1)
-data_scale2 = pd.DataFrame(scale(base_test2))
-data_scale2.columns = [s + '_norm' for s in list(base_test2.columns.values)]  # Renomer les colonnes
-
-
-
-
 # ACP
-pca = PCA(n_components=37)
-pcafit = pca.fit(data_scale2)
+pca = PCA(n_components=39)
+pcafit = pca.fit(data_scale)
 var = pca.explained_variance_ratio_
 
 ## Graph de la variance expliquée par les composantes
-pylab.ylim([-0.01,0.3])
-pylab.xlim([-1,38])
+pylab.ylim([-0.01, 0.3])
+pylab.xlim([-1, 40])
 plt.ylabel('Part de la variance expliquée')
 plt.xlabel('Composantes')
-plt.plot(var,'bo')
+plt.plot(var, 'bo')
 plt.show()
 
 ## Graph de la variance cumulée expliquée
@@ -135,26 +77,27 @@ plt.plot(np.cumsum(var))
 plt.show()
 
 ## Nouvelle coordonnées
-data_coor2 = pd.DataFrame(pca.transform(data_scale2))
-data_coor2.columns = ["Comp_" + str(l) for l in list(range(1, 38, 1))] # Renomer les colonnes
+score = pca.transform(data_scale)
+data_coor = pd.DataFrame(pca.transform(data_scale))
+data_coor.columns = ["Comp_" + str(l) for l in list(range(1, 40, 1))] # Renomer les colonnes
 
-## Enregistrement des données data_coor2 pour programme kmeans_outliers
-data_coor2.to_csv(path + '/PCA_coor.csv',index=False)
+## Enregistrement des données data_coor pour programme kmeans_outliers
+data_coor.to_csv(path + '/PCA_coor2.csv', index=False)
 
 
 ## Scatter plot sur les premiers plans de l'ACP
 
-pylab.ylim([-7,10])
-plt.boxplot(np.array(data_coor2)[:,0:37])
-plt.scatter(np.array(data_coor2)[:,0],np.array(data_coor2)[:,1])
-plt.scatter(np.array(data_coor2)[:,0],np.array(data_coor2)[:,2])
-plt.scatter(np.array(data_coor2)[:,1],np.array(data_coor2)[:,2])
+pylab.ylim([-7, 10])
+plt.boxplot(np.array(data_coor)[:, 0:39])
+plt.scatter(np.array(data_coor)[:, 0], np.array(data_coor)[:, 1])
+plt.scatter(np.array(data_coor)[:, 0], np.array(data_coor)[:, 2])
+plt.scatter(np.array(data_coor)[:, 1], np.array(data_coor)[:, 2])
 
 ## Graph des 3 premieres composantes 3D
 fig = plt.figure(1, figsize=(8, 6))
 ax = Axes3D(fig, elev=-150, azim=110)
-ax.scatter(np.array(data_coor2)[:, 0], np.array(data_coor2)[:, 1], np.array(data_coor2)[:, 2],
-cmap=plt.cm.Paired)
+ax.scatter(np.array(data_coor)[:, 0], np.array(data_coor)[:, 1], \
+           np.array(data_coor)[:, 2], cmap=plt.cm.Paired)
 ax.set_title("ACP: trois premieres composantes")
 ax.set_xlabel("Comp1")
 ax.w_xaxis.set_ticklabels([])
@@ -164,79 +107,31 @@ ax.set_zlabel("Comp3")
 ax.w_zaxis.set_ticklabels([])
 
 ## Biplot
-xvector = pca.components_[0]
+xvector = pca.components_[0] #1ere composante exprimee dans le referentiel initial des features
 yvector = pca.components_[2]
 
-xs = pca.transform(data_scale2)[:,0]
-ys = pca.transform(data_scale2)[:,2]
+xs = pca.transform(data_scale)[:, 0] #coordonnees des individus sur 1ere composante
+ys = pca.transform(data_scale)[:, 2]
 
-plt.figure(figsize=(16,8))
+plt.figure(figsize=(16, 8))
 plt.title('Représentation des variables dans les composantes 1 et 3')
 plt.xlabel('Composante 1')
 plt.ylabel('composante 3')
 
-pylab.ylim([-3,2])
-pylab.xlim([-3,6])
-for i in range(len(xvector)):
+pylab.ylim([-3, 2])
+pylab.xlim([-3, 6])
+for i in range(len(xvector)): #len(xvector) = nb features
 # arrows project features (ie columns from csv) as vectors onto PC axes
     plt.arrow(0, 0, xvector[i]*max(xs), yvector[i]*max(ys),
-              color='r', width=0.0005, head_width=0.0025,zorder=1)
+              color='r', width=0.0005, head_width=0.0025, zorder=1)
     plt.text(xvector[i]*max(xs)*1.2, yvector[i]*max(ys)*1.2,
-             list(base_test2.columns.values)[i], color='r')
+             list(base_test.columns.values)[i], color='r')
 
-for i in range(len(xs)):
+for i in range(len(xs)): #len(xs) = nb d'invidus
 # circles project documents (ie rows from csv) as points onto PC axes
-    plt.plot(xs[i], ys[i], 'g',zorder=2)
+    plt.plot(xs[i], ys[i], 'g', zorder=2)
     #plt.text(xs[i]*1.2, ys[i]*1.2, list(base_test2.index)[i], color='b')
 plt.show()
 
 
-
-## Concatenation avec les données originales
-quanti_acp = pd.concat([quanti_trans,data_coor2],axis=1)
-
-
-
-########% Clustering hierarchique
-hierar = linkage(data_coor2,'ward')
-
-## Dendogramme
-plt.figure(figsize=(25,10))
-plt.title('Dendogramme du clustering hierarchique')
-plt.xlabel('Index')
-plt.ylabel('Distance')
-dendrogram(hierar,color_threshold=200)
-
-
-## Découpage
-groupes_cah = fcluster(hierar,t=200,criterion='distance')
-plt.hist(groupes_cah)
-sum(groupes_cah==1)
-sum(groupes_cah==2)
-sum(groupes_cah==3)
-sum(groupes_cah==4)
-
-
-
-## k means
-kmeans= cluster.KMeans(n_clusters=4,max_iter =10000)
-test = kmeans.fit(pd.DataFrame(np.array(data_coor2)[:,0:10]))
-pred = kmeans.predict(pd.DataFrame(np.array(data_coor2)[:,0:10]))
-
-
-## Comparaison CAH et k-means
-pd.crosstab(groupes_cah,pred)
-
-
-## Analyses des variables sur le premier clustering
-quanti_trans['cluster'] = pd.DataFrame(pred)
-
-pd.concat([quanti_trans,pd.DataFrame(pred)])
-
-sn.boxplot(x='cluster', y='Connexion_CAEL_3m', data=quanti_trans)
-sn.boxplot(x='cluster', y='Connexion_MaBanque_3m', data=quanti_trans)
-sn.boxplot(x='cluster', y='age', data=quanti_trans)
-sn.boxplot(x='cluster', y='nb_paiement_chq_3m', data=quanti_trans)
-sn.boxplot(x='cluster', y='Agence_3m', data=quanti_trans)
-sn.boxplot(x='cluster', y='Consult_Comptes_3m', data=quanti_trans)
 
--- kmeansoutliers.py
+++ kmeansoutliers.py
@@ -24,117 +24,119 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 # Definition du chemin où sont situées les données :
-path = 'C:/Users/Richard/Documents/GitHub/Segmentation-multicanale2/Données'  
+PATH = 'C:/Users/Richard/Documents/GitHub/Segmentation-multicanale2/Données'
 
 # Import des données
-data_coor2 = pd.read_csv(path + '/PCA_coor.csv',delimiter=",")
-quanti_trans = pd.read_csv(path + '/quanti_trans.csv',delimiter=",",dtype={"IDPART_CALCULE":object})
-base_test2 =  quanti_trans.drop(['IDPART_CALCULE','nb_jours_CAEL_3m','nb_jours_MaBanque_3m','Actionsd_MaBanque_3m',
-'Lecture_mess_3m', 'Ecriture_mess_3m'], 1)
-del(quanti_trans,path)
+data_coor2 = pd.read_csv(PATH + '/PCA_coor.csv', delimiter=",")
+quanti_trans = pd.read_csv(PATH + '/v2/quanti_trans2.csv', delimiter=",", \
+                           dtype={"IDPART_CALCULE":object})
+base_test2 = quanti_trans.drop(['IDPART_CALCULE', 'Actionsd_MaBanque_3m', \
+                                'Lecture_mess_3m', 'Ecriture_mess_3m'], 1)
+del quanti_trans, PATH
 
 # 1er k means
-kmeans= cluster.KMeans(n_clusters=2000,max_iter =1,random_state=111)
-test = kmeans.fit(pd.DataFrame(np.array(data_coor2)[:,0:10]))
-pred = kmeans.predict(pd.DataFrame(np.array(data_coor2)[:,0:10]))
-data_clus = pd.concat([data_coor2,pd.DataFrame(pred)],axis=1)
-data_clus = data_clus.rename(columns={0: 'cluster'}) 
-data_clus['count'] = data_clus.groupby('cluster')['cluster'].transform('count') # Compte le nombre d'
-# observations dans 1 cluster
-data_clus_10 = data_clus[data_clus['count']>10] # Garder uniquement ceux qui contiennet plus de 10 observations
-centroid=data_clus_10.groupby('cluster').mean() # Centroids par cluster
-centroid2=np.array(centroid.drop('count',axis=1)) # Centroids contains the centroids of the clusters that gather
-# more than 10 observations
-del(centroid,data_clus_10)
+kmeans = cluster.KMeans(n_clusters=2000, max_iter=1, random_state=111)
+test = kmeans.fit(pd.DataFrame(np.array(data_coor2)[:, 0:10]))
+pred = kmeans.predict(pd.DataFrame(np.array(data_coor2)[:, 0:10]))
+data_clus = pd.concat([data_coor2, pd.DataFrame(pred)], axis=1)
+data_clus = data_clus.rename(columns={0: 'cluster'})
+data_clus['count'] = data_clus.groupby('cluster')['cluster'].transform('count')
+# Compte le nombre d' observations dans 1 cluster
+data_clus_10 = data_clus[data_clus['count'] > 10]
+# Garder uniquement ceux qui contiennet plus de 10 observations
+centroid = data_clus_10.groupby('cluster').mean() # Centroids par cluster
+centroid2 = np.array(centroid.drop('count', axis=1)) # Contains the centroids
+# of the clusters that gather more than 10 observations
+del centroid, data_clus_10
 
 # Computing euclidian distance of each observations to the nearest cluster
-test = vq(data_coor2,centroid2)
-distances= test[1] # distance de chaque observation au cluster le plus proche.
-pylab.ylim([0,7])
+test = vq(data_coor2, centroid2)
+distances = test[1] # distance de chaque observation au cluster le plus proche.
+pylab.ylim([0, 7])
 plt.boxplot(distances)
-np.percentile(distances,90)
+np.percentile(distances, 90)
 
-## Relancer un k-means en virant les observations dont la distance au cluster le plus proche est supérieure à 5
-## et en initialisant les centres précédents
-t=pd.DataFrame(distances)
-datatest = pd.concat([data_coor2,t],axis=1)
+## Relancer un k-means en virant les observations dont la distance au cluster
+# le plus proche est supérieure à 5 et en initialisant les centres précédents
+t = pd.DataFrame(distances)
+datatest = pd.concat([data_coor2, t], axis=1)
 datatest = datatest.rename(columns={0: 'distance'})
-datatest_5 = datatest[abs(datatest['distance']) <=5]
-del(distances,t)
+datatest_5 = datatest[abs(datatest['distance']) <= 5]
+del distances, t
 
 ## K means sur ces obs
 # Selection aleatoires des centres + centres de l'ancien K-means
-rand = np.zeros(shape=(2000-len(centroid2),10))
-for i in range(0,2000-len(centroid2)):
-    data_prov = pd.DataFrame(np.array(data_coor2.sample(2000,random_state=i))[:,0:10])
+rand = np.zeros(shape=(2000-len(centroid2), 10))
+for i in range(0, 2000-len(centroid2)):
+    data_prov = pd.DataFrame(np.array(data_coor2.sample(2000, random_state=i))[:, 0:10])
     rand[i] = data_prov.mean()
 
-cent= pd.DataFrame(centroid2[:,0:10])
-rand_centroid = pd.concat([cent,pd.DataFrame(rand)],axis=0)
+cent = pd.DataFrame(centroid2[:, 0:10])
+rand_centroid = pd.concat([cent, pd.DataFrame(rand)], axis=0)
 
 
-kmeans= cluster.KMeans(n_clusters=2000,init = rand_centroid)
-test = kmeans.fit(pd.DataFrame(np.array(datatest_5)[:,0:10]))
-pred = kmeans.predict(pd.DataFrame(np.array(datatest_5)[:,0:10]))
-nv_centres=test.cluster_centers_
-del(rand,cent,rand_centroid,i,datatest_5,data_prov)
+kmeans = cluster.KMeans(n_clusters=2000, init=rand_centroid)
+test = kmeans.fit(pd.DataFrame(np.array(datatest_5)[:, 0:10]))
+pred = kmeans.predict(pd.DataFrame(np.array(datatest_5)[:, 0:10]))
+nv_centres = test.cluster_centers_
+del rand, cent, rand_centroid, i, datatest_5, data_prov
 
 
 # K means sur les centres avec 1 iteration sur toutes les observation
-kmeans= cluster.KMeans(n_clusters=2000,max_iter=1,init = nv_centres)
-test = kmeans.fit(pd.DataFrame(np.array(data_coor2)[:,0:10]))
-pred = kmeans.predict(pd.DataFrame(np.array(data_coor2)[:,0:10]))
-nv_centres_pr_cah=test.cluster_centers_
-del(nv_centres)
+kmeans = cluster.KMeans(n_clusters=2000, max_iter=1, init=nv_centres)
+test = kmeans.fit(pd.DataFrame(np.array(data_coor2)[:, 0:10]))
+pred = kmeans.predict(pd.DataFrame(np.array(data_coor2)[:, 0:10]))
+nv_centres_pr_cah = test.cluster_centers_
+del nv_centres
 
 
 
 ########% Clustering hierarchique
 # Matrice des distances
 Y = pdist(nv_centres_pr_cah, 'euclidean')
-hierar = linkage(Y,'ward')
-del(Y)
+hierar = linkage(Y, 'ward')
+del Y
 
 
 
 ## Dendogramme
-plt.figure(figsize=(25,10))
+plt.figure(figsize=(25, 10))
 plt.title('Dendogramme du clustering hierarchique')
 plt.xlabel('Index')
 plt.ylabel('Distance')
-dendrogram(hierar,color_threshold=75)
+dendrogram(hierar, color_threshold=75)
 
 
 ## Découpage
-groupes_cah = fcluster(hierar,t=75,criterion='distance')
+groupes_cah = fcluster(hierar, t=75, criterion='distance')
 plt.hist(groupes_cah)
-sum(groupes_cah==1)
-sum(groupes_cah==2)
-sum(groupes_cah==3)
-sum(groupes_cah==4)
-del(hierar)
+sum(groupes_cah == 1)
+sum(groupes_cah == 2)
+sum(groupes_cah == 3)
+sum(groupes_cah == 4)
+del hierar
 
 
 # Calcul des centroids de la CAH:
-centres_cah = pd.concat([pd.DataFrame(nv_centres_pr_cah),pd.DataFrame(groupes_cah)],axis=1)
+centres_cah = pd.concat([pd.DataFrame(nv_centres_pr_cah), pd.DataFrame(groupes_cah)], axis=1)
 centres_cah.columns = ["Comp_" + str(l) for l in list(range(1, 11, 1))] + ['Cluster']
 centres_cah_mean = centres_cah.groupby('Cluster').mean()
-del(centres_cah,nv_centres_pr_cah)
+del centres_cah, nv_centres_pr_cah
 
 ## Dernier k-means avec initialisation des centres de la CAH
-kmeans= cluster.KMeans(n_clusters=4,max_iter=10000,init = centres_cah_mean)
-test = kmeans.fit(pd.DataFrame(np.array(data_coor2)[:,0:10]))
-pred = kmeans.predict(pd.DataFrame(np.array(data_coor2)[:,0:10]))
+kmeans = cluster.KMeans(n_clusters=4, max_iter=10000, init=centres_cah_mean)
+test = kmeans.fit(pd.DataFrame(np.array(data_coor2)[:, 0:10]))
+pred = kmeans.predict(pd.DataFrame(np.array(data_coor2)[:, 0:10]))
 plt.hist(pred)
-del(centres_cah_mean)
+del centres_cah_mean
 
 # Recuperation des variables initiales
-clustered_data = pd.concat([base_test2,pd.DataFrame(pred)],axis=1)
+clustered_data = pd.concat([base_test2, pd.DataFrame(pred)], axis=1)
 clustered_data = clustered_data.rename(columns={0: 'cluster'})
-sum(pred==0)
-sum(pred==1)
-sum(pred==2)
-sum(pred==3)
+sum(pred == 0)
+sum(pred == 1)
+sum(pred == 2)
+sum(pred == 3)
 
 
 
@@ -142,24 +144,33 @@ sum(pred==3)
 analyses_kmeans = clustered_data.groupby('cluster').mean()
 
 # Code pour boxplots
-sub= list(range(1, 38, 1))
-plt.figure(figsize=(40,40))
-var_names = list(clustered_data.columns.values)[0:37]
-for i in range(0,37):
-    plt.subplot(8,5,sub[i])
+sub = list(range(1, 40, 1))
+plt.figure(figsize=(40, 40))
+var_names = list(clustered_data.columns.values)[0:39]
+for i in range(0, 39):
+    plt.subplot(8, 5, sub[i])
     sn.boxplot(x='cluster', y=var_names[i], data=clustered_data)
-    
+
 
 
 
 ## Représentation dans le plan de l'ACP des clusters
 
 # Base acp avec les clusters
-C = np.array(pd.concat([pd.DataFrame(np.array(data_coor2)[:,0:10]),clustered_data['cluster']],axis=1))
+C = np.array(pd.concat([pd.DataFrame(np.array(data_coor2)[:, 0:10]), \
+                                     clustered_data['cluster']], axis=1))
+
+# changement des couleurs
+clus = np.char.mod('%d', C[:, 10])
+colors = np.array(['testttttttttttt'] * 20000)
+colors[clus == '0'] = 'lime'
+colors[clus == '1'] = 'darkmagenta'
+colors[clus == '2'] = 'royalblue'
+colors[clus == '3'] = 'cyan'
 
 fig = plt.figure(1, figsize=(8, 6))
 ax = Axes3D(fig, elev=-150, azim=110)
-ax.scatter(C[:, 0], C[:, 1], C[:, 2], c=(C[:,10] *2),cmap=plt.cm.Paired)
+ax.scatter(C[:, 0], C[:, 1], C[:, 2], c=colors, cmap=plt.cm.Paired)
 ax.set_title("ACP: trois premieres composantes")
 ax.set_xlabel("Comp1")
 ax.w_xaxis.set_ticklabels([])
@@ -167,3 +178,11 @@ ax.set_ylabel("Comp2")
 ax.w_yaxis.set_ticklabels([])
 ax.set_zlabel("Comp3")
 ax.w_zaxis.set_ticklabels([])
+
+
+plt.savefig('Plot_3_comp.png', dpi=600)
+
+
+
+
+
